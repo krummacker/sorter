184740fd9c408db30a2d084423b95756beccd7bc
Dirk Krummacker <rasen_02cider@icloud.com>
2022-12-29 17:21:37 +0100

    test sorter-perfcheck where possible

---
+++ sorter-perfcheck_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"testing"
+)
+
+// Tests the createRandomInts function.
+func TestCreateRandomInts(t *testing.T) {
+
+	tests := map[string]struct {
+		size int
+		want int
+	}{
+		"zero_size": {
+			size: 0,
+			want: 0,
+		},
+		"size_one": {
+			size: 1,
+			want: 1,
+		},
+		"size_thousand": {
+			size: 1000,
+			want: 1000,
+		},
+	}
+
+	for name, test := range tests {
+		got := createRandomInts(test.size)
+		if len(got) != test.want {
+			t.Errorf("%s: got %v but want %v", name, got, test.want)
+		}
+	}
+}
+
+// Tests the average function.
+func TestAverage(t *testing.T) {
+
+	tests := map[string]struct {
+		input []int
+		want  int
+	}{
+		"zero_size": {
+			input: []int{},
+			want:  0,
+		},
+		"size_one": {
+			input: []int{42},
+			want:  42,
+		},
+		"size_many": {
+			input: []int{1, 2, 3, 4, 5, 6, 7},
+			want:  4,
+		},
+	}
+
+	for name, test := range tests {
+		got := average(test.input)
+		if got != test.want {
+			t.Errorf("%s: got %v but want %v", name, got, test.want)
+		}
+	}
+}
